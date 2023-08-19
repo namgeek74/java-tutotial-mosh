bde3f9f73ac84b4d1103ec9c80af128bcf518648
namnd <namnguyennd7428@gmail.com>
2023-08-19 14:55:31 +0700

    basic calculate read and write with system

--- src/com/helloworld/Main.java
+++ src/com/helloworld/Main.java
@@ -1,35 +1,29 @@
 package com.helloworld;
 
 import java.awt.*;
+import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] numbers = new int[5];
-        numbers[0] = 0;
-        numbers[1] = 1;
-        System.out.println(Arrays.toString(numbers));
-        String[] strs = new String[2];
-        boolean[] arrBoolean = new boolean[3];
+        final byte PERCENT = 100;
+        final byte MONTHS_IN_YEAR = 12;
 
-        System.out.println(Arrays.toString(strs));
-        System.out.println(Arrays.toString(arrBoolean));
-        int[] intArrNumber = {2, 3, 5, 7, 2};
-        Arrays.sort(intArrNumber);
-        System.out.println(intArrNumber.length);
-        System.out.println(Arrays.toString(intArrNumber));
+        Scanner scanner = new Scanner(System.in);
 
-        // multi dimension array
-        int[][] nums = {{1, 2, 3}, {4, 5}};
-        // nums[1][3] = 7;
-        System.out.println(nums.length);
-        System.out.println(nums[0].length);
-        System.out.println(nums[1].length);
+        System.out.print("Your principal: ");
+        double principal = scanner.nextDouble();
+        System.out.print("Your rate: ");
+        double rate = scanner.nextDouble();
+        System.out.print("Your period: ");
+        double period = scanner.nextDouble();
 
-        // constant
-        final float PI = 3.14F;
-        // PI = 1;  Cannot assign a value to final variable 'PI'
+        double monthlyInterest = rate / (PERCENT * MONTHS_IN_YEAR);
+
+        double result = principal * ((monthlyInterest * (Math.pow((1 + monthlyInterest), period))) / (Math.pow((1 + monthlyInterest), period) - 1));
+        System.out.println("Your mortgage is: " + result);
     }
 }
