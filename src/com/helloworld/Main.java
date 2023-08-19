9a886da75cfa1d618633338605ea6b086da473c5
namnd <namnguyennd7428@gmail.com>
2023-08-20 01:15:34 +0700

    method and recursion

--- src/com/helloworld/Main.java
+++ src/com/helloworld/Main.java
@@ -9,21 +9,42 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        final byte PERCENT = 100;
-        final byte MONTHS_IN_YEAR = 12;
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Your principal: ");
-        double principal = scanner.nextDouble();
-        System.out.print("Your rate: ");
-        double rate = scanner.nextDouble();
-        System.out.print("Your period: ");
-        double period = scanner.nextDouble();
-
-        double monthlyInterest = rate / (PERCENT * MONTHS_IN_YEAR);
-
-        double result = principal * ((monthlyInterest * (Math.pow((1 + monthlyInterest), period))) / (Math.pow((1 + monthlyInterest), period) - 1));
-        System.out.println("Your mortgage is: " + result);
+        myMethod();
+        int myNum1 = plusMethod(8, 5);
+        double myNum2 = plusMethod(4.3, 6.26);
+        System.out.println("int: " + myNum1);
+        System.out.println("double: " + myNum2);
+        System.out.println("recursion");
+        int result = sum(5, 10);
+        System.out.println(result);
     }
+
+    public static void myMethod() {
+        System.out.println("print in my method");
+    }
+
+    static int plusMethodInt(int x, int y) {
+        return x + y;
+    }
+
+    static double plusMethodDouble(double x, double y) {
+        return x + y;
+    }
+
+    static int plusMethod(int x, int y) {
+        return x + y;
+    }
+
+    static double plusMethod(double x, double y) {
+        return x + y;
+    }
+
+    public static int sum(int start, int end) {
+        if (end > start) {
+            return end + sum(start, end - 1);
+        } else {
+            return end;
+        }
+    }
+
 }
