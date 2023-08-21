797a7a9670f6f7ba610397447c2411a461d60229
namnd <namnguyennd7428@gmail.com>
2023-08-21 13:21:22 +0700

    inner class

---
+++ src/com/helloworld/InnerClass.java
@@ -0,0 +1,17 @@
+package com.helloworld;
+
+public class InnerClass {
+    int x = 10;
+
+    class ChildClass {
+        int y = 5;
+
+        public int myInnerMethod() {
+            return x;
+        }
+    }
+
+    static class ChildStaticClass {
+        int z = 15;
+    }
+}
--- src/com/helloworld/Main.java
+++ src/com/helloworld/Main.java
@@ -5,13 +5,13 @@ import com.helloworld.otherpackage.First;
 
 public class Main {
     public static void main(String[] args) {
-        Animal myAnimal = new Animal();  // Create a Animal object
-        Animal myPig = new Pig();  // Create a Pig object
-        Animal myDog = new Dog();  // Create a Dog object
-        myAnimal.animalSound();
-        myPig.animalSound();
-        myDog.animalSound();
-
+        InnerClass innerClass = new InnerClass();
+        InnerClass.ChildClass childClass = innerClass.new ChildClass();
+        System.out.println(innerClass.x);
+        System.out.println(childClass.y);
+        System.out.println(childClass.myInnerMethod());
+        InnerClass.ChildStaticClass childStaticClass = new InnerClass.ChildStaticClass();
+        System.out.println(childStaticClass.z);
     }
 
 }
