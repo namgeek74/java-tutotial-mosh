651b89e48850557f9fda6f1c8d8b353a2e300fc8
namnd <namnguyennd7428@gmail.com>
2023-08-26 07:41:19 +0700

    string builder and string buffer

---
+++ src/com/helloworld/string/BuilderAndBuffer.java
@@ -0,0 +1,20 @@
+package com.helloworld.string;
+
+// StringBuilder was introduced in Java 1.5 as a replacement for StringBuffer.
+
+public class BuilderAndBuffer {
+    public static void show() {
+        // String is immutable, it create new object
+        String immutable = "abc";
+        immutable = immutable + "def";
+
+        // StringBuffer and StringBuilder are mutable, we are using append method to modify them, no creating the new object
+        StringBuffer sb = new StringBuffer("abc");
+        sb.append("def");
+
+        // Simply put, the StringBuffer is a thread-safe implementation and therefore slower than the StringBuilder.
+
+        StringBuilder stringBuilder = new StringBuilder("abc");
+        stringBuilder.append("def");
+    }
+}
