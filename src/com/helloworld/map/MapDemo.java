5f8afd9f6a60d9fe7532fecd57ae92e4ec114cce
namnd <namnguyennd7428@gmail.com>
2023-08-26 07:28:15 +0700

    hashtable and compare with hashmap

--- src/com/helloworld/map/MapDemo.java
+++ src/com/helloworld/map/MapDemo.java
@@ -1,10 +1,15 @@
 package com.helloworld.map;
 
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 // Map is not a subtype of Collection
 // Map interface -> HashMap class -> LinkedHashMap class (allow null key and null value)
 // Map interface -> SortedMap interface -> TreeMap class (not allow null key and null value)
-
-import java.util.*;
 
 public class MapDemo {
     public static void show() {
@@ -34,5 +39,23 @@ public class MapDemo {
         treeMap.put("key0", null);
 
         System.out.println(treeMap);
+
+        Hashtable<Word, String> hashtable = new Hashtable<>();
+        Word key1 = new Word("key 1");
+        hashtable.put(key1, "value 1");
+        Word key0 = new Word("key 0");
+        hashtable.put(key0, "value 0");
+        Word key2 = new Word("key 2");
+        // the following line led to NullPointerException
+        //        hashtable.put(key2, null);
+        System.out.println(hashtable);
+
+        // Compare HashMap and Hashtable
+        // Hashtable is thread-safe and can be shared between multiple threads in the application
+        // HashMap is not synchronized, therefore it's faster and uses less memory than Hashtable
+        // HashMap allows adding one Entry with null as key as well as many entries with null as value
+        // In contrast, Hashtable doesn't allow null at all
+        // It is worth mentioning that since JDK 1.8, Hashtable has been deprecated
+        // However, ConcurrentHashMap is a great Hashtable replacement
     }
 }
---
+++ src/com/helloworld/map/Word.java
@@ -0,0 +1,9 @@
+package com.helloworld.map;
+
+public class Word {
+    public String name;
+
+    public Word(String name) {
+        this.name = name;
+    }
+}
