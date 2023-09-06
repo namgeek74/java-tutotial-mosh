eab4a15b797214ef3d40c2c8a1d1f90c5a058d64
namnd <namnguyennd7428@gmail.com>
2023-09-06 14:28:05 +0700

    SOLID principles

---
+++ src/com/helloworld/demo/SolidPrinciples.java
@@ -0,0 +1,19 @@
+package com.helloworld.demo;
+
+// SOLID principles:
+
+// Single Responsibility: a class should only have one responsibility
+
+// Open/Closed: Open for extension, closed for modification
+
+// Liskov Substitution: if class A is a subtype of class B
+// we should be able to replace B with A without disrupting the behavior of our program
+
+// Interface Segregation: larger interfaces should be split into smaller ones
+// By doing so, we can ensure that implementing classes only need to be concerned about the methods that are of interest to them
+
+// Dependency Inversion: The principle of dependency inversion refers to the decoupling of software modules
+// This way, instead of high-level modules depending on low-level modules, both will depend on abstractions
+
+public class SolidPrinciples {
+}
